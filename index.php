8c738cbd960418ccced357835bb0becde58fcad3
Rami Serapian <rami.serapian@gmail.com>
2018-10-27 11:13:04 -0400

    First commit TP2

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php include 'header.html' ?>
+<nav class="navbar navbar-default">
+<div class="container-fluid">
+  <!-- Films and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <!-- <a class="navbar-brand" href="https://www.arsenal.com">Films</a> -->
+    <span class="navbar-brand" href="https://www.arsenal.com">Films</span>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Nos films <span class="sr-only">(current)</span></a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cat&eacute;gories<span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Action</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">One more separated link</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Devenir membre</a></li>
+
+      <form id="form-login" name="form-login" action="login.php" method="POST">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Connexion</a></li>
+      </form>
+    </ul>
+  </div><!-- /.navbar-collapse -->
+</div><!-- /.container-fluid -->
+</nav>
+
+<div style="float: left; display: inline-block; margin: 20px; padding: 20px;">
+
+<div class="card text-center" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Bay Michael</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Action</h6>
+    <h6 class="card-subtitle mb-2 text-muted" style="font-style: italic;">$ 6.99</h6>
+  </div>
+</div>
+
+<div class="card text-center" style="width: 18rem; margin: auto;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Bay Michael</p>
+    <p class="card-text">Action</p>
+    <p class="card-text" style="font-style: italic;">$ 6.99</p>
+  </div>
+</div>
+
+</div>
+<?php include 'footer.html'?>
---
+++ login.php
@@ -0,0 +1,56 @@
+<?php include 'header.html' ?>
+<link rel="stylesheet" href="css/tp2.css" />
+<script type="text/javascript" src="js/tp2.js"></script>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 col-md-offset-3">
+			<div class="panel panel-login">
+				<!-- panel heading -->
+				<div class="panel-heading">
+					<div class="row">
+						<div class="col-xs-6">
+							<a href="#" class="active" id="login-form-link">Connexion</a>
+						</div>
+					</div>
+					<hr>
+				</div>
+				<!-- panel body -->
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-lg-12">
+							<form id="login-form" action="index.php" method="post" role="form" style="display: block;">
+								<!-- username text -->
+								<div class="form-group">
+									<input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
+								</div>
+								<!-- password text -->
+								<div class="form-group">
+									<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
+								</div>
+								<!-- login submit -->
+								<div class="form-group">
+									<div class="row">
+										<div class="col-sm-6 col-sm-offset-3">
+											<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Se connecter">
+										</div>
+									</div>
+								</div>
+								<!-- cancel submit -->
+								<div class="form-group">
+									<div class="row">
+										<div class="col-sm-6 col-sm-offset-3">
+											<input type="submit" name="login-cancel" id="login-cancel" tabindex="4" class="form-control btn btn-cancel" value="Annuler">
+										</div>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include 'footer.html' ?>
