03dca7e524bb35be7a2cd48675ae77c8957ced10
rsn <rsn@notixia.com>
2018-11-09 16:38:55 -0500

    film_store.sql

--- db/film_store.sql
+++ db/film_store.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
 --
 -- Host: localhost    Database: film_store
 -- ------------------------------------------------------
--- Server version	5.7.17
+-- Server version	5.7.24-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -42,6 +42,37 @@ LOCK TABLES `film` WRITE;
 INSERT INTO `film` VALUES (1,'Suicide Squad','Ayer David','Action',1000,6.99),(2,'Transformers','Bay Michael','Action',1000,6.99),(3,'The Shining','Kubrich Stanley','Horreur',2000,14.99),(4,'Mon oncle','Tati Jacques','Comedie',2500,12.97);
 /*!40000 ALTER TABLE `film` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  `admin` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `username_2` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (7,'rami','rami@rami.com','9e580b8971ebb5f0d9401cb05a82c20e8b45935b98afb408378f3131a67e8a93','8d7476cf464ccb5f5b300bc6e96dad69d6a39fa2273792a5341a994479f19c25',NULL),(6,'fadi','fadi@fadi.com','9a7a734f231eadf17264e2c81418311edc6b3f009be4dbdf5e19d54c4c4f7a4e','b1451f1e56027903623806425a5a8025025e2c62a2d9b1a301c11b7c2f5323df',NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -52,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-09  8:17:14
+-- Dump completed on 2018-11-09 16:37:31
