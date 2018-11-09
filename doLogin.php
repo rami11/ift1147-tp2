c1968aa6a67d7a0ef944d06bb36e5572a44776a2
rsn <rsn@notixia.com>
2018-11-09 14:29:46 -0500

    doLogin

--- doLogin.php
+++ doLogin.php
@@ -3,32 +3,42 @@
  * @Author: rsn
  * @Date:   2018-11-09 11:29:53
  * @Last Modified by:   rsn
- * @Last Modified time: 2018-11-09 11:51:59
+ * @Last Modified time: 2018-11-09 14:27:56
  */
+require_once("includes/functions.php");
 require_once("db/connection.inc.php");
 
-$usename = $_POST["username"];
+// $hello = = $_POST["hello"];
+// printf("Hello: %s<br>", $hello);
+
+$username = $_POST["username"];
 printf("username: %s<br>", $username);
 $password = $_POST["password"];
 printf("password: %s<br>", $password);
 
 $saltQuery = "SELECT salt FROM users WHERE username = '$username';";
+printf( "query: %s<br>", $saltQuery);
 $result = $connection->query($saltQuery);
+printf( "result: %d<br>", $result);
 # you'll want some error handling in production code :)
 # see http://php.net/manual/en/function.mysql-query.php Example #2 for the general error handling template
-$row = $result->fetch_assoc($result);
-$salt = $row['salt'];
+$salt = "";
+while ($row = $result->fetch_assoc()) {
+	//printf( "row: %s<br>", $row);
+	$salt = $row["salt"];
+}
 
+printf( "salt: %s<br>", $salt);
 $saltedPassword =  $password . $salt;
 
 $hashedPassword = hash('sha256', $saltedPassword);
 
-$query = "SELECT * FROM users WHERE username = '$username' and password = '$hashedPassword';";
-printf("Query: %s<br>", $query);
+$sql = "SELECT * FROM users WHERE username = '$username' and password = '$hashedPassword';";
+printf("Query: %s<br>", $sql);
 
-$count = $connection->query();
+$count = $connection->query($sql);
 printf("Result: %d<br>", $count);
 
-
+redirect_to("index.php");
 # if nonzero query return then successful login
 ?>
